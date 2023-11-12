c8dffc7bae9ed5330e207d06039aa58d35d06a3e
ImantsS <91641460+ImantsS@users.noreply.github.com>
2023-11-11 16:10:37 -0800

    test vue change

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,30 @@
 <template>
-  <main>
-    <div class="intro">
-      <h1>Nuxt Toolbox</h1>
-      <p>
-        Hi 👋! This template gives you a
-        <a href="https://nuxtjs.org/">Nuxt</a> app with the scaffolding for
-        <a href="https://www.netlify.com/products/functions/"
-          >Netlify Functions</a
-        >, <a href="https://www.netlify.com/products/forms/">Forms</a>, and
-        <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>. Our
-        aim was to give you the code you would need to hit the ground running
-        with a few fun features.
-      </p>
-
-      <p>
-        You can find the code for this project on GitHub at
-        <a href="https://github.com/netlify-templates/nuxt-toolbox"
-          >https://github.com/netlify-templates/nuxt-toolbox</a
-        >! Happy coding!
-      </p>
-    </div>
-    <FeedbackForm />
-    <JokeBlock />
-  </main>
+  <div v-html="helpContent"></div>
 </template>
+<script>
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      helpContent: ''
+    };
+  },
+  mounted() {
+    this.fetchHelpContent();
+  },
+  methods: {
+    async fetchHelpContent() {
+      try {
+        const response = await axios.get('https://cad.onshape.com/help');
+        this.helpContent = response.data;
+      } catch (error) {
+        console.error('Error fetching help content:', error);
+      }
+    }
+  }
+};
+</script>
 <style>
 * {
   font-family: 'Helvetica', sans-serif;
